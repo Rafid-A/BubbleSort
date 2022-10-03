5f9e39020dc39ea6c083f514efdcfb6fc7fbed3a
Rafid Ahmed <rafid.ahmed@student.allenisd.org>
2022-10-03 16:43:32 -0500

    Adding BubbleSort

---
+++ main.swift
@@ -0,0 +1,34 @@
+
+var unsortedIntegers = [String]()
+
+func readWords() -> [String] {
+var line : String?
+repeat {
+    line = readLine()
+    if line != nil {
+        unsortedIntegers.append(line!)
+    }
+} while line != nil
+return unsortedIntegers
+}
+ 
+func bubbleSort(unsortedIntegers: [String]) -> [String] {
+
+    var sortingIntegers = readWords()
+
+    for _ in 0 ..< sortingIntegers.count - 1 {
+        for j in 0 ..< sortingIntegers.count - 1 {
+            if (sortingIntegers[j] > sortingIntegers[j + 1]) {
+                let temp = sortingIntegers[j]
+                sortingIntegers[j] = sortingIntegers[j + 1]
+                sortingIntegers[j + 1] = temp
+            }
+        }
+    }
+    return sortingIntegers
+}       
+let sortedIntegers = bubbleSort(unsortedIntegers: unsortedIntegers)
+for element in sortedIntegers {
+    print(element)
+}
+
